fb748a01778ca3fe9f919e4544e9e314028e4232
Rizvi Faiz Syed <rizvifaiz1073@gmail.com>
2024-08-18 22:24:02 +0530

    pdf uploader

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,27 +1,34 @@
+import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 export default function Header() {
   return (
-    <div className="flex justify-center ">
-      <div className="flex justify-center border-white rounded-2xl p-8">
-        <ul className="flex gap-4">
-          <li className="underline-animation flex transition-all hover:font-bold hover:text-red-600 cursor-pointer ">
-            Home
-          </li>
-          <li className="underline-animation flex transition-all hover:font-bold hover:text-red-600 cursor-pointer">
-            About
-          </li>
-          <li className="underline-animation flex transition-all hover:font-bold hover:text-red-600 cursor-pointer">
-            Dashboard
-          </li>
-          <li className="underline-animation flex transition-all hover:font-bold hover:text-red-600 cursor-pointer">
-            Contact
-          </li>
-          <li className="underline-animation flex transition-all hover:font-bold hover:text-red-600 cursor-pointer">
-            Help
-          </li>
-        </ul>
+    <div className="flex justify-between items-center p-4">
+         <div className="flex-shrink-0">
+        <img src='/next.svg' alt="Logo" className="h-5" />
       </div>
+      <div className="flex-1 flex justify-center">                           
+        <div className="border-white rounded-2xl p-8">
+          <ul className="flex gap-4">
+            <li className="underline-animation flex transition-all text-xl hover:font-extrabold hover:text-red-600 cursor-pointer">
+              Home
+            </li>
+            <li className="underline-animation flex transition-all text-xl hover:font-extrabold hover:text-red-600 cursor-pointer">
+              About
+            </li>
+            <li className="underline-animation flex transition-all text-xl hover:font-extrabold hover:text-red-600 cursor-pointer">
+              Dashboard
+            </li>
+            <li className="underline-animation flex transition-all text-xl hover:font-extrabold hover:text-red-600 cursor-pointer">
+              Contact
+            </li>
+            <li className="underline-animation flex transition-all text-xl hover:font-extrabold hover:text-red-600 cursor-pointer">
+              Help
+            </li>
+          </ul>
+        </div>
+      </div>
+      <UserButton />
     </div>
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import PdfUpload from "./components/PdfUpload";
+
 
 export default function Home() {
   return (
     <div>
       <Header />
       <h1>Welcome to my website</h1>
+      <PdfUpload />
     </div>
   );
 }
