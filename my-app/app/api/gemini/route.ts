5e1087ef647f8e17bf7eec93f454a1277754c281
Rizvi Faiz Syed <rizvifaiz1073@gmail.com>
2024-08-20 22:49:28 +0530

    slight changes

--- my-app/app/api/gemini/route.ts
+++ my-app/app/api/gemini/route.ts
@@ -7,12 +7,13 @@ export async function POST(req: NextRequest) {
 
     const API_KEY = process.env.GEMINI_API_KEY;
     const GEMINI_API_URL = process.env.GEMINI_API_URL;
+    console.log(GEMINI_API_URL);
 
     if (!API_KEY || !GEMINI_API_URL) {
       return NextResponse.json({ error: 'API key or URL is not defined' }, { status: 500 });
     }
 
-    const url = `${GEMINI_API_URL}?key=${API_KEY}`;
+    const url = `${GEMINI_API_URL}key=${API_KEY}`;
     const body = JSON.stringify({
       contents: [
         {
@@ -25,23 +26,21 @@ export async function POST(req: NextRequest) {
       ],
     });
 
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyA4DVrF0PDLs0ZIAFl8d7Nr1Bag8FRAh6s`, {
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: body,
     });
-    // const res = await response.json();
-
-    // console.log(res["data"]["candidates"]["0"]["content"]["parts"]["0"]["text"]);
+   
     if (!response.ok) {
       const errorDetail = await response.text();
       return NextResponse.json({ error: `Failed to get response from Gemini API: ${errorDetail}` }, { status: response.status });
     }
 
     const result = await response.json();
-    console.log(result)
+    // console.log(result)
     return NextResponse.json({ answer: result["candidates"]["0"]["content"]["parts"]["0"]["text"] });
   } catch (error) {
     if (error instanceof Error) {
